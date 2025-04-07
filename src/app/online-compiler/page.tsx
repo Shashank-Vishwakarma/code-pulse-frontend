0397aedf1ffdd7b4f475880cae4b9b62212285ca
Shashank <shashank78680@gmail.com>
2025-04-08 01:22:34 +0530

    setup for compiler

--- src/app/online-compiler/page.tsx
+++ src/app/online-compiler/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Header from '@/components/shared/header/Header'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import {FaPython} from "react-icons/fa"
 import { RiJavascriptFill } from "react-icons/ri";
 import { PiFileCppBold } from "react-icons/pi";
@@ -15,36 +15,79 @@ import {
     ResizablePanel,
     ResizablePanelGroup
 } from "@/components/ui/resizable"
+import { cn } from '@/lib/utils';
+import { toast } from 'sonner';
 
 const supportedLanguages = [
     {
         language: "python",
-        icon: <FaPython className='w-4 h-4 mr-2'/>,
-        defaultCode: "print(\"Hello World\")"
+        icon: <FaPython className='w-8 h-8'/>,
+        defaultCode: localStorage.getItem("python-code-pulse-compiler-code") || "print(\"Hello World\")"
     },
     {
-        language: "javaScript",
-        icon: <RiJavascriptFill className='w-4 h-4 mr-2'/>,
-        defaultCode: "console.log(\"Hello World\")"
+        language: "javascript",
+        icon: <RiJavascriptFill className='w-8 h-8'/>,
+        defaultCode: localStorage.getItem("javascript-code-pulse-compiler-code") || "console.log(\"Hello World\")"
     },
     {
         language: "cpp",
-        icon: <PiFileCppBold  className='w-4 h-4 mr-2'/>,
-        defaultCode: "#include <iostream>\nusing namespace std;\nint main() {\n    cout << \"Hello World\";\n    return 0;\n}"
+        icon: <PiFileCppBold  className='w-8 h-8'/>,
+        defaultCode: localStorage.getItem("cpp-code-pulse-compiler-code") || "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n    cout << \"Hello World\";\n    return 0;\n}"
     },
     {
         language: "java",
-        icon: <LiaJava  className='w-4 h-4 mr-2'/>,
-        defaultCode: "class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println(\"Hello World\");\n    }\n}"
+        icon: <LiaJava  className='w-8 h-8'/>,
+        defaultCode: localStorage.getItem("java-code-pulse-compiler-code") || "class Main {\n    public static void main(String[] args) {\n        System.out.println(\"Hello World\");\n    }\n}"
     },
     {
         language: "go",
-        icon: <FaGolang className='w-4 h-4 mr-2'/>,
-        defaultCode: "package main\n\nimport \"fmt\"\n\nfunc main() {\n    fmt.Println(\"Hello World\")\n}"
+        icon: <FaGolang className='w-8 h-8'/>,
+        defaultCode: localStorage.getItem("go-code-pulse-compiler-code") || "package main\n\nimport \"fmt\"\n\nfunc main() {\n    fmt.Println(\"Hello World\")\n}"
     }
 ]
 
 export default function OnlineCompiler() {
+    const [selectedLanguage, setSelectedLanguage] = useState(supportedLanguages[0].language);
+    const [code, setCode] = useState(supportedLanguages[0].defaultCode);
+    const [consoleOutput, setConsoleOutput] = useState("");
+
+    const editorRef = useRef(null)
+
+    const handleEditorMount = (editor: any) => {
+        editorRef.current = editor;
+    }
+
+    const saveCodeToLocalStorage = (language: string, code: string) => {
+        localStorage.setItem(`${language}-code-pulse-compiler-code`, code);
+    }
+
+    const clearCodeFromLocalStorage = (language: string) => {
+        localStorage.removeItem(`${language}-code-pulse-compiler-code`);
+    }
+
+    const handleChangeLanguage = (language: string) => {
+        setSelectedLanguage(language);
+    }
+
+    const handleClearConsole = () => {
+        setConsoleOutput("")
+    }
+
+    const handleClearEditor = () => {
+        setCode("");
+        clearCodeFromLocalStorage(selectedLanguage);
+    }
+
+    const handleCodeRun = async () => {
+        if(selectedLanguage === "" || code === "") {
+            toast.error("Please select a language and write some code to run...");
+            return
+        }
+
+        saveCodeToLocalStorage(selectedLanguage, code);
+        setConsoleOutput("Code run successful!");
+    }
+
     return (
         <div className='h-screen bg-gradient-to-br from-gray-900 to-blue-900'>
             <div className='w-64'>
@@ -58,9 +101,16 @@ export default function OnlineCompiler() {
                         {supportedLanguages.map((language, index) => (
                             <div
                                 key={index}
-                                className="flex items-center gap-4 p-4 rounded-xl shadow-sm bg-white border hover:shadow-md cursor-pointer transition"
+                                className={cn(
+                                    "flex items-center gap-4 p-4 rounded-xl shadow-sm bg-white hover:shadow-md cursor-pointer transition",
+                                    selectedLanguage == language.language ? "bg-blue-600 text-white" : ""
+                                )}
+                                onClick={()=>{
+                                    handleChangeLanguage(language.language);
+                                    setCode(language.defaultCode);
+                                }}
                             >
-                                <div className="p-2 bg-gray-100 rounded-full">
+                                <div className="rounded-full">
                                     {language.icon}
                                 </div>
                             </div>
@@ -76,7 +126,10 @@ export default function OnlineCompiler() {
                             <Editor
                                 height="100%"
                                 theme="vs-dark"
-                                language='python'
+                                language={selectedLanguage}
+                                onChange={(value) => setCode(value || "")}
+                                onMount={handleEditorMount}
+                                value={code}
                                 options={{
                                     minimap: { enabled: false },
                                     fontSize: 14,
@@ -91,18 +144,19 @@ export default function OnlineCompiler() {
                             <div className="flex items-center justify-between px-4 py-2 bg-gray-200 border-b border-gray-300">
                                 <h3 className="text-base font-medium text-gray-700">Console Output</h3>
                                 <div className="flex gap-2">
-                                    <Button variant="outline">
+                                    <Button variant="outline" onClick={handleClearConsole}>
                                         <Trash className="h-3 w-3" />
                                         Clear Console
                                     </Button>
 
-                                    <Button variant="outline">
+                                    <Button variant="outline" onClick={handleClearEditor}>
                                         <Trash className="h-3 w-3" />
                                         Clear Editor
                                     </Button>
 
                                     <Button
                                         className="flex items-center justify-center gap-2 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                        onClick={handleCodeRun}
                                     >
                                         <Play className="h-4 w-4" />
                                         Run
@@ -110,8 +164,8 @@ export default function OnlineCompiler() {
                                 </div>
                             </div>
 
-                            <div className="flex-1 p-4 h-full font-mono text-sm bg-black text-green-400 overflow-auto whitespace-pre-wrap">
-                                Run your code to see output here...
+                            <div className="flex-1 p-4 h-full font-mono text-sm bg-black text-white overflow-auto whitespace-pre-wrap">
+                                {consoleOutput || "Run your code to see output here..."}
                             </div>
                         </ResizablePanel>
                     </ResizablePanelGroup>
